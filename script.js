3cb196836d1f80d1f5ecd95822f88780ba85f45e
Ujjwal Acharya <81429137+ujjwalshiva@users.noreply.github.com>
2021-11-18 23:46:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,35 @@
 console.log("Initializing App...");
 
-//Formula: [P x R x (1+R) ^N]/ [(1+R) ^ (N-1)]
+document.addEventListener("contextmenu", function(event){
+    event.preventDefault();    
+}, false);
 
 function calculate() {
 
-    let total=document.querySelector('#total').value;
-    let attended=document.querySelector('#attended').value;
-    let bunk=document.querySelector('#bunk').value;
+    let total=parseInt(document.querySelector('#total').value);
+    let attended=parseInt(document.querySelector('#attended').value);
+    let bunk=parseInt(document.querySelector('#bunk').value);
 
     if (attended<=total) {
         let currentatt = (parseFloat((attended/total*100), 10)).toFixed(2);
 
         console.log(currentatt);
-    
-        let bunkatt = (parseFloat((attended/(total + bunk))*1000, 10).toFixed(2));
 
+        bunktot = total + bunk;
+    
+        let bunkatt = (parseFloat(((attended/bunktot)*100), 10).toFixed(2));
+
+        let nobunkatt = (parseFloat(((attended+bunk)/bunktot)*100, 10).toFixed(2));
+        
         console.log(bunkatt);
 
-        document.querySelector('#final').innerHTML=`Current Attendance: ${currentatt} <br> You're attendance will drop to ${bunkatt}`;
+        console.log(nobunkatt);
+
+        document.querySelector('#final').innerHTML=`Current Attendance: ${currentatt} <br> If you bunk, then value drops to <span style="color:#FA5F55">${bunkatt}</span> <br> Else, value rises to <span style="color:#69F0AE">${nobunkatt}</span>`;
     }
 
     else {
         document.querySelector('#final').innerHTML=`Total Classes > Attended Classes`;
     }
-    
-
-    //Old Formula Below
-    //const interest = (amount * (interestrate * 0.01)) / months;
-    //let payment = ((amount / months) + interest).toFixed(0);
-
-
-    /*let amount=document.querySelector('#amount').value;
-    let interestrate=document.querySelector('#interestrate').value;
-    let months=document.querySelector('#months').value;
-
-    if (amount=='' || interestrate==''|| months=='') {
-        document.querySelector('#final').innerHTML=`<h4 style='color:#ef5350'>Enter Values</h4>`;
-    }
-
-    else {
-        const interest = interestrate/1200;
-    
-        let top = Math.pow((1+interest),months);
-        let bottom = top - 1;
-        let ratio = top/bottom;
-        let emi = (amount * interest * ratio).toFixed(0);
-
-        document.querySelector('#final').innerHTML=`Your EMI is ${emi}`;      
-    
-        console.log(amount, interestrate, months);
-    }*/
-
     
 }
